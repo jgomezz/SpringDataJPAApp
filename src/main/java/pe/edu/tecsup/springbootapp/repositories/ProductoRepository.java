f455fcda772fcbc6835e4a9a9b492ca2f87891f3
jgomezz <jgomezz@gmail.com>
2024-04-28 13:14:26 -0500

    Update testUpdate() with JPA

--- src/main/java/pe/edu/tecsup/springbootapp/repositories/ProductoRepository.java
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/ProductoRepository.java
@@ -11,18 +11,4 @@ import java.util.List;
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 
 	List<Producto> findByNombre(String nombre) throws Exception;
-
-	/*
-	List<Producto> findAll() throws Exception;
-
-	List<Producto> findByName(String nombre) throws Exception;
-
-	Producto findById(Long id) throws Exception;
-
-	void save(Producto producto) throws Exception;
-	
-	void update(Long id, String nombreProducto) throws Exception;
-
-	void deleteById(Long id) throws Exception;
-	*/
 }
