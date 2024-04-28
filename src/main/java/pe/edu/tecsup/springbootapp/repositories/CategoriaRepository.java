2c44933540c07058e9d2ab3e08c29c0a85518666
jgomezz <jgomezz@gmail.com>
2024-04-28 13:16:34 -0500

    Optimize code

--- src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepository.java
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepository.java
@@ -4,12 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import pe.edu.tecsup.springbootapp.entities.Categoria;
+import pe.edu.tecsup.springbootapp.entities.Producto;
 
 import java.util.List;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
-    @Query("SELECT c FROM Categoria c WHERE c.nombre = :name")
-    List<Categoria> findByNombre(@Param("name") String name);
+    //@Query("SELECT c FROM Categoria c WHERE c.nombre = :name")
+    //List<Categoria> findByNombre(@Param("name") String name);
+
+    List<Categoria> findByNombre(String nombre) throws Exception;
 
 }
